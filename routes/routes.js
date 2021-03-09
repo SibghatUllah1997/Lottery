1fc156d3c93091c593dfccb93acfd7f32882c972
HM Sibghat Ullah <59176632+SibghatUllah1997@users.noreply.github.com>
2021-03-09 17:11:28 +0500

    adding node and react files

---
+++ routes/routes.js
@@ -0,0 +1,10 @@
+const Router = require("express").Router();
+const bodyparser = require("body-parser");
+const controller = require("../controller/controller");
+
+Router.get("/api/v1/owner", controller.owner);
+Router.post("/api/v1/startlottery", controller.startLottery);
+Router.get("/api/v1/status", controller.status);
+Router.get("/api/v1/drawwinner", controller.drawWinner);
+
+module.exports = Router;
